914e7f88914fd75bc7f536cff84e4c5dfcc6a23d
RubenCruchaga <rc103008@wgcloud.org>
2023-10-25 11:41:47 -0500

    fixed the question problem

--- euro.py
+++ euro.py
@@ -1,16 +1,16 @@
 dollar= 0
 euro= 0 
-yesNo= input("Enter the Dollar amount to be converted(yes,no): ")
+yesNo= input(" do you whatr to convert Dollars to Euros.(yes,no): ")
 #wihel loop to do it mulitpul times
 
 while yesNo == "yes":
     if yesNo == "yes":
         # ask what amout og moneyu
-        dollar = float(input("How maney dollars do you what to covert: "))
+        dollar = float(input("Enter the Dollar amount to be converted: "))
         euro = dollar * .94540
 
         print("Your Euro amout is " + str(round(euro, 2)))
-        yesNo= input("Enter the Dollar amount to be converted(yes,no): ")
+        yesNo= input(" do you what to convert Dollars to Euros agen.(yes,no): ")
     elif yesNo == "no":
     #tell person what happend
         print("You have side no you hit F5 to try agen")
