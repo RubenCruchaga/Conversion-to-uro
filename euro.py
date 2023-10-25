b685a5548b564f5bd18af6949ee3652310073582
RubenCruchaga <rc103008@wgcloud.org>
2023-10-25 11:47:42 -0500

    fixed the questions agen

--- euro.py
+++ euro.py
@@ -1,6 +1,6 @@
 dollar= 0
 euro= 0 
-yesNo= input(" do you whatr to convert Dollars to Euros.(yes,no): ")
+yesNo= input("Would you like to convert dollars to euros?(yes,no): ")
 #wihel loop to do it mulitpul times
 
 while yesNo == "yes":
@@ -10,7 +10,7 @@ while yesNo == "yes":
         euro = dollar * .94540
 
         print("Your Euro amout is " + str(round(euro, 2)))
-        yesNo= input(" do you what to convert Dollars to Euros agen.(yes,no): ")
+        yesNo= input("Would you like to convert dollars to euros?(yes,no): ")
     elif yesNo == "no":
     #tell person what happend
         print("You have side no you hit F5 to try agen")
